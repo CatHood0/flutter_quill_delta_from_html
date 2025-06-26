a45752cec82070f0a1dad20b76bbcc2431cb9245
Alban LECUIVRE <lecuivre.alban@gmail.com>
2025-06-26 10:50:42 +0200

    feat: handle italic, underline and bold

--- lib/parser/html_utils.dart
+++ lib/parser/html_utils.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_quill_delta_from_html/parser/indent_parser.dart';
+
 import 'colors.dart';
 import 'font_size_parser.dart';
 import 'line_height_parser.dart';
@@ -110,6 +111,20 @@ Map<String, dynamic> parseStyleAttribute(String style) {
             //ignore error (i.e. 'line-height: inherit;')
           }
           break;
+        case 'font-style':
+          if (value == 'italic') {
+            attributes['italic'] = true;
+          }
+          break;
+        case 'text-decoration':
+          if (value == 'underline') {
+            attributes['underline'] = true;
+          }
+          break;
+        case 'font-weight':
+          if (value == 'bold') {
+            attributes['bold'] = true;
+          }
         default:
           break;
       }
